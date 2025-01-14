01e52692a96c5f2fd34776bebf363df533333942
Imre Bartis <imbartis@gmail.com>
2025-01-14 14:37:18 +0200

    combine CSS modules with Tailwind

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -2,26 +2,28 @@ import Image from 'next/image'
 
 import { Product } from './products'
 
+import styles from './ProductCard.module.css'
+
 interface Props {
   product: Product
 }
 
 export const ProductCard = ({ product }: Props) => {
   return (
-    <div className='@container'>
-      <div className='flex flex-col @md:flex-row'>
-        <div className='w-full @md:w-1/4'>
+    <div className={styles.card}>
+      <div className={styles.cardContainer}>
+        <div className={styles.imageContainer}>
           <Image
             src={product.image}
             alt={product.title}
             width={300}
             height={300}
-            className='w-full h-auto rounded-tl-2xl rounded-tr-2xl @md:rounded-tr-none rounded-bl-none @md:rounded-bl-2xl'
+            className={styles.image}
           />
         </div>
-        <div className='w-full @md:w-3/4 pl-4 border-b-2 border-gray-400 rounded-br-2xl rounded-bl-2xl @md:rounded-bl-none border-l-2 @md:border-t-2 rounded-tr-none @md:rounded-tr-2xl border-r-2 @md:border-l-transparent'>
-          <h1 className='text-2xl mt-4'>{product.title}</h1>
-          <p className='text-lg mb-4 italic'>{product.price}</p>
+        <div className={styles.infoContainer}>
+          <h1 className={styles.title}>{product.title}</h1>
+          <p className={styles.price}>{product.price}</p>
         </div>
       </div>
     </div>
